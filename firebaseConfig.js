9a3a648b8f09a39f86393f4ede532987d9ba203a
Aaro <aaro.heroja@gmail.com>
2026-02-26 20:07:11 +0200

    refactor: cleaned code

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Edit your own firebase config here https://firebase.google.com
+export const firebaseConfig = {
+    apiKey: "AIzaSyCwjycLBjwLnaX7ukzcP8vGHa9TPL7jdRw",
+    authDomain: "pestinvalintakone.firebaseapp.com",
+    projectId: "pestinvalintakone",
+    storageBucket: "pestinvalintakone.firebasestorage.app",
+    messagingSenderId: "605582430584",
+    appId: "1:605582430584:web:e2b5ba11cbf9dfc359f88d"
+};
--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// Edit your own firebase config here https://firebase.google.com
-const firebaseConfig = {
-    apiKey: "AIzaSyCwjycLBjwLnaX7ukzcP8vGHa9TPL7jdRw",
-    authDomain: "pestinvalintakone.firebaseapp.com",
-    projectId: "pestinvalintakone",
-    storageBucket: "pestinvalintakone.firebasestorage.app",
-    messagingSenderId: "605582430584",
-    appId: "1:605582430584:web:e2b5ba11cbf9dfc359f88d"
-};
+import { firebaseConfig } from "/firebaseConfig.js"
 
 document.addEventListener("DOMContentLoaded", async () => {
     firebase.initializeApp(firebaseConfig);
@@ -54,7 +46,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
                 selectedJobs.push(job);
                 div.classList.add("selected");
-                console.log("Selected job:", selectedJobs);
             });
         });
     }
